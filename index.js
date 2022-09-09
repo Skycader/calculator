36b96c710eee4432be9ea609ab57aa6ed088b613
skycader <skycader@github.com>
2022-09-09 23:54:39 +0300

    Added fixes for double signs and brackets

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const clickHandler = (event) => {
   let sym = event.target.innerHTML;
   switch (sym) {
     case "=":
-      calc();
+      if (document.querySelector(".input").value) calc();
       break;
     case "AC":
       document.querySelector(".input").value = "";
@@ -13,16 +13,39 @@ const clickHandler = (event) => {
         .value.slice(0, -1);
       break;
     case "()":
+      let data = document.querySelector(".input").value
+      if (data[data.length - 1].match(/[+|\-|÷|×]/)) {
+        add("(");
+      } else {
+        if ((data.includes("("))&&(data.match(/[(]/g).length == (data.match(/[\)]/g)||[]).length+1)) {
+        add(")");
+        }
+      }
       break;
     default:
-      add(sym);
+        let error = 0
+        let symcopy = sym.replaceAll("×", "*").replaceAll("÷", "/")
+        let copy = document.querySelector(".input").value.replaceAll("×", "*").replaceAll("÷", "/")
+        let expr = copy+=symcopy+"1"
+       
+        try {
+            eval(expr)
+        } catch(e) {
+            error=1
+        }
+        if (!error) {
+            add(sym)
+        }
   }
 };
 
 const add = (value) => (document.querySelector(".input").value += value);
 const calc = () =>
   (document.querySelector(".input").value = eval(
-    document.querySelector(".input").value.replaceAll("×", "*").replaceAll("÷","/")
+    document
+      .querySelector(".input")
+      .value.replaceAll("×", "*")
+      .replaceAll("÷", "/")
   ));
 
 document.querySelector("#root").addEventListener("click", clickHandler);
