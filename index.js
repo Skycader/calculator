42a293c21d07623083d99ce046a4032dd39279c0
skycader <skycader@github.com>
2022-09-14 15:27:48 +0300

    Some bug fixes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const clickHandler = (event) => {
     case "+":
     case "×":
     case "÷":
-      if (!data[data.length - 1]?.match(/[+|÷|×]/) && !isNaN(num)) {
+      if (!data[data.length - 1]?.match(/[+|÷|×]/)) {
         add(sym);
       }
       break;
